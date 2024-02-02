c6de4192748105278f4c2590ba7913146515cc4e
Med Wahbi <wahbisj@gmail.com>
2024-02-02 18:39:36 +0100

    creation d'une request DELETE only used by admin pour supprimer une category by ID .

--- server/controllers/categorysController.js
+++ server/controllers/categorysController.js
@@ -35,3 +35,22 @@ module.exports.getAllCategoryCtrl = asyncHandler (async (req,res)=>{
     const categories = await Category.find();
     res.status(200).json(categories);
 })
+
+
+
+// -------------------------------------------------------------
+// *   @disc       Delete category bi ID
+// *   @Router     api/categorys/:id
+// *   @methode    DELETE
+// *   @access     priveat (only by admin)
+// -------------------------------------------------------------
+module.exports.deleteCategoryCtrl = asyncHandler ( async (req,res)=>{
+    const category = await Category.findById({_id:req.params.id});
+    if(!category){
+        return res.status(400).json({message:"category not found."})
+    };
+
+    await Category.findByIdAndDelete({_id:req.params.id});
+
+    res.status(200).json({message:"category has been deleted successfully .",categoryId : category._id})
+})
--- server/routes/categorysRoute.js
+++ server/routes/categorysRoute.js
@@ -1,12 +1,18 @@
 const express = require ('express');
 const router = express.Router();
-const {createCategoryCtrl,getAllCategoryCtrl} = require ("../controllers/categorysController");
+const {createCategoryCtrl,getAllCategoryCtrl, deleteCategoryCtrl} = require ("../controllers/categorysController");
 const { verifyTokenAndAdminHeaders } = require('../middlewares/verifyToken');
+const validateObjectId = require('../middlewares/validateObjectId.js');
 
-
+// api/categories/
 router.route('/')
 .post(verifyTokenAndAdminHeaders,createCategoryCtrl)
 .get(getAllCategoryCtrl)
+
+// api/categories/:id
+router.route('/:id')
+.delete(validateObjectId,verifyTokenAndAdminHeaders,deleteCategoryCtrl)
+
 
 
 module.exports=router;
