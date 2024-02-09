75b877277fb57f3e30a36be70704df82bf48a21d
Med Wahbi <wahbisj@gmail.com>
2024-02-09 15:56:11 +0100

    creation de la structure du home page

--- frontend/blog/src/pages/home/Home.jsx
+++ frontend/blog/src/pages/home/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import './home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-hero-header">
+        <div className="home-hero-header-layout">
+          <h1 className='home-title'>Welcome to Blog</h1>
+        </div>
+      </div>
+
+      <div className="home-latest-post">Latest Posts</div>
+      <div className="home-container">
+        <div className="post-list">post list</div>
+        <div className="post-sidebar">side bar</div>
+      </div>
+    </section>
   )
 }
 
